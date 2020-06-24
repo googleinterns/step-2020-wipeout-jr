4ad739a4056915179f09c0f1505efc93a59d6ed3
Ankita Mitra <ankitamitra@berkeley.edu>
2020-06-24 19:07:19 +0000

    added try-catch block

--- portfolio/src/main/java/com/google/sps/servlets/BookServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BookServlet.java
@@ -1,5 +1,5 @@
 package com.google.sps.servlets;
- 
+
 import com.google.gson.Gson;
 import com.google.sps.data.Book;
 import java.io.IOException;
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
 /**
  * Returns book titles and reviews as a JSON array, e.g. [{"title": Othello, "reviews": {"Nice",
  * "Bad"}}]
@@ -21,49 +21,51 @@ import javax.servlet.http.HttpServletResponse;
 public class BookServlet extends HttpServlet {
   private Collection<Book> books;
   private Collection<String> test;
- 
+
   @Override
-  public void init() {
+  public void init() throws ServletException {
     books = new ArrayList<>();
     test = new ArrayList<>();
     readBooks();
   }
- 
-  private void readBooks() {
+
+  private void readBooks() throws ServletException {
     String path = "/WEB-INF/"
         + "20_books.csv";
-    String content = new Scanner(getServletContext().getResourceAsStream(path))
-                         .useDelimiter("\\Z")
-                         .next()
-                         .replaceAll("[\\r\\n]+", "");
-    String[] lines = content.split("NEXTBOOK"); // lines[i] represents one row of the file
- 
-    String current_title = "";
-    Book.Builder current_builder = Book.builder().title("null");
- 
-    for (int i = 1; i < lines.length; i++) {
-      String[] cells = lines[i].split(",");
-      String title = cells[0];
-      String genre = cells[8];
-      String review = cells[13];
-      if (current_title.equals(title)) {
-        // add review:
-        current_builder.addReview(review);
-      } else {
-        // close old book:
-        if (i != 1) {
-          Book book = current_builder.build();
-          books.add(book);
+    try (Scanner scanner =
+             new Scanner(getServletContext().getResourceAsStream(path)).useDelimiter("\\Z")) {
+      String content = scanner.next().replaceAll("[\\r\\n]+", "");
+      String[] lines = content.split("NEXTBOOK"); // lines[i] represents one row of the file
+
+      String current_title = "";
+      Book.Builder current_builder = Book.builder().title("null");
+
+      for (int i = 1; i < lines.length; i++) {
+        String[] cells = lines[i].split(",");
+        String title = cells[0];
+        String genre = cells[8];
+        String review = cells[13];
+        if (current_title.equals(title)) {
+          // add review:
+          current_builder.addReview(review);
+        } else {
+          // close old book:
+          if (i != 1) {
+            Book book = current_builder.build();
+            books.add(book);
+          }
+          // start building new book
+          current_builder = Book.builder().title(title).genre(genre).addReview(review);
+          current_title = title;
         }
-        // start building new book
-        current_builder = Book.builder().title(title).genre(genre).addReview(review);
-        current_title = title;
       }
+      Book book = current_builder.build();
+      books.add(book);
+    } catch (Exception e) {
+      throw new ServletException("Error reading CSV file", e);
     }
-    Book book = current_builder.build();
-    books.add(book);
   }
- 
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,7 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
