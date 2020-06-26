47b4c179355b0762f56e5f23d3d59dde08dd8d17
Danya Gao <dgao@wellesley.edu>
2020-06-26 09:04:00 +0000

    changed book list component

--- portfolio/src/main/webapp/book-list/book-list.component.js
+++ portfolio/src/main/webapp/book-list/book-list.component.js
@@ -6,15 +6,19 @@ angular.
       // TODO(danya): change this to call the book servlet to get book data (we don't want phones lol)
       this.phones = [
         {
+          id: 1,
           name: 'Nexus S',
           snippet: 'Fast just got faster with Nexus S.'
         }, {
+          id: 2,
           name: 'Motorola XOOM™ with Wi-Fi',
           snippet: 'The Next, Next Generation tablet.'
         }, {
+          id: 3,
           name: 'MOTOROLA XOOM™',
           snippet: 'The Next, Next Generation tablet.'
         }
       ];
     }
   });
+  
