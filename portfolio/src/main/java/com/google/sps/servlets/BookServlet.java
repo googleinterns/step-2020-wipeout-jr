5b9a9677d5fa0b9f9932138e841d097d6ff4c0ae
Ankita Mitra <ankitamitra@berkeley.edu>
2020-06-30 01:12:52 +0000

    delete deadcode

--- portfolio/src/main/java/com/google/sps/servlets/BookServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BookServlet.java
@@ -30,47 +30,6 @@ public class BookServlet extends HttpServlet {
     books = reader.makeBookList();
   }
 
-//   private HashMap<Integer, Book> readBooks() throws ServletException {
-//     HashMap<Integer, Book> allBooks = new HashMap<Integer, Book>();
-//     String path = "/WEB-INF/"
-//         + "20_books.csv";
-//     try (Scanner scanner =
-//              new Scanner(getServletContext().getResourceAsStream(path)).useDelimiter("\\Z")) {
-//       String content = scanner.next().replaceAll("[\\r\\n]+", "");
-//       String[] lines = content.split("NEXTBOOK"); // lines[i] represents one row of the file
-
-//       String current_title = "";
-//       Book.Builder current_builder = Book.builder().title("null");
-//       int currentId = 0;
-
-//       for (int i = 1; i < lines.length; i++) {
-//         String[] cells = lines[i].split(",");
-//         String title = cells[0];
-//         String genre = cells[8];
-//         String review = cells[13];
-//         if (current_title.equals(title)) {
-//           // add review:
-//           current_builder.addReview(review);
-//         } else {
-//           // close old book:
-//           if (i != 1) {
-//             Book book = current_builder.build();
-//             allBooks.put(currentId++, book);
-//           }
-//           // start building new book
-//           current_builder = Book.builder().title(title).genre(genre).addReview(review);
-//           current_title = title;
-//         }
-//       }
-//       Book book = current_builder.build();
-//       allBooks.put(currentId, book);
-//     } catch (Exception e) {
-//       throw new ServletException("Error reading CSV file", e);
-//     }
-//     return allBooks;
-// >>>>>>> master
-//   }
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
