faf7b67a79f76ef6617f0d1e79aa928a2b2eef4a
aamanhey <amanhey@googl.com>
2020-07-08 09:43:01 +0000

    Converts the String Buffer from the servlet get request into a map.

--- portfolio/src/main/java/com/google/sps/data/StringBufToMap.java
+++ portfolio/src/main/java/com/google/sps/data/StringBufToMap.java
@@ -1,93 +1,185 @@
 package com.google.sps.data;
 
 import java.io.BufferedReader;
-import java.util.Map;
+import java.lang.RuntimeException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
-public class StringBufToMap{
-    public Map<String,String> map;
-    private String input;
+public class StringBufToMap {
+  public Map<String, Object> outputMap;
+  private String inputString;
 
-    public Map<String,String> toMap(StringBuffer sb){
-        this.input = sb.toString();
-        map = new HashMap<String,String>();
-        int quotationCnt = 0;
-        int curlyCnt = 0;
-        String key = "";
-        String value = "";
+  public Map<String, Object> toMap(StringBuffer sb) {
+    this.inputString = sb.toString();
+    this.outputMap = getMap(inputString);
+    return outputMap;
+  }
 
-        int index = 0;
-        char character;
-        Map newMap;
-        //Map newMap = new HashMap<String,String>;
-        String sub;
-        int incrementor = 1;
-        int keyValueBalance = 0;
-        int inputLength = input.length();
-        boolean isKey = true;
+  private Map<String, Object> getMap(String parent) {
+    String key = "";
+    Object value = null;
+    int index = 0;
+    char character;
+    Map newMap = new HashMap<String, Object>();
+    int incrementor = 1;
+    int parentLength = parent.length();
+    boolean isKey = true;
 
-        while(index < inputLength){
-            /*System.out.println("");
-            System.out.println("Index:" + index);
-            System.out.println("Map: " + map);
-            System.out.println("isKey: " + isKey);*/
+    while (index < parentLength) {
+      character = parent.charAt(index);
 
-            character = input.charAt(index);
-            //System.out.println("Char: " + character);
-            incrementor = 1;
-            if(key.length() > 0 && value.length() > 0){
-                map.put(key,value);
-                key = "";
-                value = "";
-            }
-            switch (character) {
-                case '{': {
-                    //newMap = new Map<String,String>();
-                    break;
-                }
-                case '\"': {
-                    //need to add 1 because these operations are inclusive
-                    int nextIndex = input.indexOf(character,index+1);
-                    //System.out.println("next index: " + nextIndex);
-                    if(nextIndex > 0){
-                        sub = input.substring(index+1,nextIndex);
-                        //System.out.println("sub: "+sub);
-                        incrementor = nextIndex - index + 1;
-                        if(isKey){
-                            key = sub;
-                        }else{
-                            value = sub;
-                        }
-                    }
-                    break;
-                }
-                case ':': {
-                    isKey = !isKey;
-                    break;
-                }
-                case ',': {
-                    isKey = true;
-                    break;
-                }
-                case '[': {
-                    //
-                    break;
-                }
-                case ']': {
-                    //
-                    break;
-                }
-                case '}': {
-                    //
-                    break;
-                }
-                default: {
-                    incrementor = 1;
-                    break;
-                }
-            }
-            index += incrementor;
+      incrementor = 1;
+      switch (character) {
+        case '{': {
+          // either the start of the map or a new sub map
+          if (index != 0) {
+            value = getMap(getEnd(index + 1, character, parent));
+
+          } else {
+            newMap = getMap(getEnd(index + 1, character, parent));
+          }
+          incrementor = getIncriment(index, character, parent);
+          break;
         }
-        return this.map;
+        case '[': {
+          // a new sublist
+          value = getList(getEnd(index + 1, character, parent));
+          incrementor = getIncriment(index, character, parent);
+          break;
+        }
+        case '\"': {
+          // get the entire field (e.g. "title", "kind")
+          // need to add 1 because these operations are inclusive
+          int nextIndex = parent.indexOf(character, index + 1);
+          if (nextIndex > 0) {
+            String sub = parent.substring(index + 1, nextIndex);
+            incrementor = nextIndex - index + 1;
+            if (isKey) {
+              key = sub;
+            } else {
+              value = sub;
+            }
+          }
+          break;
+        }
+        case ':': {
+          isKey = false;
+          break;
+        }
+        case ',': {
+          isKey = true;
+          break;
+        }
+        default: {
+          incrementor = 1;
+          break;
+        }
+      }
+      index += incrementor;
+      if (key.length() > 0 && value != null) {
+        newMap.put(key, value);
+        key = "";
+        value = null;
+        isKey = true;
+      }
     }
+    return newMap;
+  }
+
+  private ArrayList<Object> getList(String parent) {
+    Object value = null;
+    int index = 0;
+    char character;
+    ArrayList<Object> newList = new ArrayList<Object>();
+    int incrementor = 1;
+    int parentLength = parent.length();
+
+    while (index < parentLength) {
+      character = parent.charAt(index);
+      incrementor = 1;
+      switch (character) {
+        case '{': {
+          value = getMap(getEnd(index + 1, character, parent));
+          incrementor = getIncriment(index, character, parent);
+          break;
+        }
+        case '[': {
+          value = getList(getEnd(index + 1, character, parent));
+          incrementor = getIncriment(index, character, parent);
+          break;
+        }
+        case '\"': {
+          int nextIndex = parent.indexOf(character, index + 1);
+          if (nextIndex > 0) {
+            value = parent.substring(index + 1, nextIndex);
+            incrementor = nextIndex - index + 1;
+          }
+          break;
+        }
+        default: {
+          incrementor = 1;
+          break;
+        }
+      }
+      index += incrementor;
+      if (value != null) {
+        newList.add(value);
+        value = null;
+      }
+    }
+    return newList;
+  }
+
+  private int getIncriment(int index, char character, String parent) {
+    int counterPartIndex = indexOfOpp(index + 1, character, parent);
+    if (counterPartIndex > index) {
+      return counterPartIndex - index;
+    }
+    return 1;
+  }
+
+  private String getEnd(int start, char character, String parent) {
+    return parent.substring(start, indexOfOpp(start, character, parent));
+  }
+
+  private int indexOfOpp(int start, char character, String parent) {
+    int strLength = parent.length();
+    int index = start;
+    int balance = 1;
+    char currentChar;
+    char opposite = opposite(character);
+
+    while (index < strLength) {
+      if (balance < 0 || balance > strLength) {
+        throw new RuntimeException("Balance thrown to unreasonable levels");
+      }
+      currentChar = parent.charAt(index);
+      if (currentChar == character) {
+        balance++;
+      } else if (currentChar == opposite) {
+        balance--;
+      }
+      if (balance == 0 && currentChar == opposite) {
+        return index;
+      }
+      index++;
+    }
+    throw new RuntimeException("Could not find the end of the map/array.");
+  }
+
+  private char opposite(char c) {
+    if (c == '{') {
+      return '}';
+    } else if (c == '}') {
+      return '{';
+    } else if (c == '[') {
+      return ']';
+    } else if (c == ']') {
+      return '[';
+    } else {
+      throw new RuntimeException("Function can only accept {}[] values");
+    }
+  }
 }
+
--- portfolio/src/test/java/com/google/sps/StringBufToMapTest.java
+++ portfolio/src/test/java/com/google/sps/StringBufToMapTest.java
@@ -1,7 +1,10 @@
 import com.google.sps.data.StringBufToMap;
 import java.io.BufferedReader;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,43 +12,57 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** This is a test class for StringBufToMap.java
-*/
+/**
+ * This is a test class for StringBufToMap.java
+ */
 
 @RunWith(JUnit4.class)
 public final class StringBufToMapTest {
+  // TODO: change strings to immutable lists
   @Test
-  public void stringValues(){
-      Map<String,String> expected = new HashMap<String,String>();
-      expected.put("TITLE","title");
-      expected.put("GENRE","genre");
-      expected.put("CATEGORIES","categories");
-      expected.put("AUTHOR","author");
-      expected.put("LANGUAGE","language");
-      expected.put("DESCRIPTION","description");
-      expected.put("INFO_LINK","infoLink");
-      expected.put("PAGE_COUNT","pageCount");
-      expected.put("PUBLISH_DATE","publishedDate");
-      expected.put("PUBLISHER","publisher");
-      expected.put("MATURITY_RATING","maturityRating");
+  public void stringValues() {
+    // tests for String Buffers with only single string values
+    Map<String, Object> expected = new HashMap<String, Object>();
+    expected.put("TITLE", "title");
+    expected.put("GENRE", "genre");
+    expected.put("CATEGORIES", "categories");
+    expected.put("AUTHOR", "author");
+    expected.put("LANGUAGE", "language");
+    expected.put("DESCRIPTION", "description");
+    expected.put("INFO_LINK", "infoLink");
+    expected.put("PAGE_COUNT", "pageCount");
+    expected.put("PUBLISH_DATE", "publishDate");
+    expected.put("PUBLISHER", "publisher");
+    expected.put("MATURITY_RATING", "maturityRating");
 
-    StringBuffer sb =  new StringBuffer();
-    String content = "{\"TITLE\": \"title\",\"GENRE\": \"genre\",\"CATEGORIES\": \"categories\",\"AUTHOR\": \"author\",\"LANGUAGE\": \"language\",\"DESCRIPTION\": \"description\",\"INFO_LINK\": \"infoLink\",\"PAGE_COUNT\": \"pageCount\",\"PUBLISH_DATE\": \"publishedDate\",\"PUBLISHER\": \"publisher\",\"MATURITY_RATING\": \"maturityRating\"}";
+    StringBuffer sb = new StringBuffer();
+    String content = "{"
+        + "\"TITLE\": \"title\","
+        + "\"GENRE\": \"genre\","
+        + "\"CATEGORIES\": \"categories\","
+        + "\"AUTHOR\": \"author\","
+        + "\"LANGUAGE\": \"language\","
+        + "\"DESCRIPTION\": \"description\","
+        + "\"INFO_LINK\": \"infoLink\","
+        + "\"PAGE_COUNT\": \"pageCount\","
+        + "\"PUBLISH_DATE\": \"publishDate\","
+        + "\"PUBLISHER\": \"publisher\","
+        + "\"MATURITY_RATING\": \"maturityRating\"}";
     sb.append(content);
-    /*Tests when all the values are only strings
-    */
-    StringBufToMap translate = new StringBufToMap();
-    Map<String,String> actual = translate.toMap(sb);
-    System.out.println("Translated: " + actual);
-    System.out.println("Expected: " + expected);
+    // Tests when all the values are only strings
 
-    //checks that the entities retrieved by the key and by passing the entity are the same
-    Assert.assertEquals(expected, actual);
-    //checks all fields are the same as original
-    for(String key : expected.keySet()){
-         Assert.assertEquals(actual.get(key),expected.get(key));
+    StringBufToMap translate = new StringBufToMap();
+    Map<String, Object> actual = translate.toMap(sb);
+    ;
+
+    // checks all fields are the same as original
+    for (Object value : expected.values()) {
+      Assert.assertEquals(true, actual.containsValue(value));
     }
-    Assert.assertEquals(expected.size(),actual.size());
+    for (String key : expected.keySet()) {
+      Assert.assertEquals(true, actual.containsKey(key));
+    }
+    Assert.assertEquals(expected.size(), actual.size());
   }
 }
 
