d84c679be8d5f8f5c3320b04ba665cd740f60f47
Ankita Mitra <ankitamitra@berkeley.edu>
2020-07-09 14:26:17 +0000

    add javadoc

--- portfolio/src/main/java/com/google/sps/data/GenreRecommender.java
+++ portfolio/src/main/java/com/google/sps/data/GenreRecommender.java
@@ -17,6 +17,12 @@ public class GenreRecommender {
   private Map<Book, Set<String>> bookToGenres;
   private Map<String, Set<Book>> genreToBooks;
 
+  /** 
+   * Constructor that takes in a list of books 
+   * and populates class variables appropriately.
+   *
+   * @param books: List of Book objects for the recommendation algorithm
+   */ 
   public GenreRecommender(List<Book> books) {
     this.books = books;
     bookToGenres = new HashMap<Book, Set<String>>();
@@ -33,15 +39,29 @@ public class GenreRecommender {
       }
     }
   }
-
-  private Set<String> getGenreSet(String genreString) {
+ 
+  /**
+  * Helper method that takes in the list of genres as a ";"-separated string
+  * and returns a set of genres 
+  * 
+  * @param genreString: ";"-separated string of genres
+  * @return ImmutableSet of genres
+  */
+  private ImmutableSet<String> getGenreSet(String genreString) {
     Set<String> genres = new HashSet<String>();
     for (String genre : genreString.toLowerCase().split(";")) {
       genres.add(genre);
     }
-    return genres;
+    return ImmutableSet.copyOf(genres);
   }
 
+  /**
+  * Takes in a Book object and returns a set of genres 
+  * of that book
+  * 
+  * @param book: Book object
+  * @return ImmutableSet of genres of that book
+  */
   public ImmutableSet<String> getGenres(Book book) {
     Set<String> genres = bookToGenres.get(book);
     if (genres == null) {
@@ -50,6 +70,13 @@ public class GenreRecommender {
     return ImmutableSet.copyOf(genres);
   }
 
+  /**
+  * Takes in a set of genres and returns a set of Book objects
+  * that have exactly the genres from the set. 
+  * 
+  * @param genres: Set of genres (as Strings)
+  * @return ImmutableSet of books that have exactly these genres
+  */
   public ImmutableSet<Book> getBooksWithExactGenres(Set<String> genres) {
     Set<Book> matchingBooks = new HashSet<Book>();
     for (Book book : bookToGenres.keySet()) {
