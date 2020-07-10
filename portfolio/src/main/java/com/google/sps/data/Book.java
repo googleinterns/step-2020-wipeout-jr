aac01b7afb59224dc0d51b4aeb7558635356b9b5
Ankita Mitra <ankitamitra@berkeley.edu>
2020-07-10 14:28:52 +0000

    fix typo

--- portfolio/src/main/java/com/google/sps/data/Book.java
+++ portfolio/src/main/java/com/google/sps/data/Book.java
@@ -29,7 +29,7 @@ public abstract class Book {
     }
 
     /**
-     *In this function, we use replaceAll instead of enconding/decoding
+     *In this function, we use replaceAll instead of encoding/decoding
      * methods because the bad characters do not come in during our encoding,
      * but rather, already exist in the file that we're reading from.
      * The two replacements in the first line are in fact, different
