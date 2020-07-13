de00d51b34be2f23f6ebad2f9f66f9e21056574b
aamanhey <amanhey@googl.com>
2020-07-13 16:58:14 +0000

    New class for books from the API and adjusted the enum to contain more fields

--- portfolio/src/main/java/com/google/sps/data/BookFieldsEnum.java
+++ portfolio/src/main/java/com/google/sps/data/BookFieldsEnum.java
@@ -1,28 +1,30 @@
 package com.google.sps.data;
- 
+
 import java.lang.Enum;
 import java.util.HashMap;
 import java.util.Map;
- 
+
 public enum BookFieldsEnum {
   TITLE("title"),
   GENRE("genre"),
-  CATEGORIES("categories"),
-  AUTHOR("author"),
+  CATEGORIES("categories"), // arraylist
+  AUTHORS("authors"), // arraylist
   LANGUAGE("language"),
   DESCRIPTION("description"),
   INFO_LINK("infoLink"),
   PAGE_COUNT("pageCount"),
   PUBLISH_DATE("publishedDate"),
   PUBLISHER("publisher"),
-  MATURITY_RATING("maturityRating");
- 
+  MATURITY_RATING("maturityRating"),
+  THUMBNAIL("thumbnail"), // image link
+  ISBN("isbn"); // can use as an id
+
   private final String javaScriptProperty;
- 
+
   private BookFieldsEnum(String javaScriptProperty) {
     this.javaScriptProperty = javaScriptProperty;
   }
- 
+
   public String getJSProperty() {
     return this.javaScriptProperty;
   }
