61bb51186332b58892f917df0a5a1b3d6fcf5ed8
aamanhey <amanhey@google.com>
2020-07-24 17:57:24 +0000

    Short term fix for book to isbn mapping issue

---
+++ portfolio/src/main/java/com/google/sps/data/IsbnReader.java
@@ -0,0 +1,40 @@
+package com.google.sps.data;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.ImmutableSet;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * Class that takes in an InputStream as the constructor parameter
+ * and populates a map with the book titles and isbns
+ */
+public class IsbnReader {
+  private final File file;
+  
+  public IsbnReader(String path) {
+    this.file = new File(path);
+  }
+
+  public ImmutableMap<String, String> makeIsbnMap() throws IOException {
+    ImmutableMap.Builder<String, String> isbnMapBuilder = new Builder<String, String>();
+    try (Scanner scanner = new Scanner(file, "utf-8").useDelimiter("\n")) {
+      while(scanner.hasNextLine()){
+        String[] content = scanner.nextLine().split(",");
+        String title = content[0];
+        String isbn = content[1].replaceAll("\\s+","");
+        if(title != "book_title"){
+            isbnMapBuilder.put(title,isbn);
+        }
+      }
+    } catch (Exception ex) {
+      throw new IOException("Error reading CSV file", ex);
+    }
+    ImmutableMap<String, String> isbnMap = isbnMapBuilder.build();
+    return isbnMap;
+  }
+}
+
---
+++ portfolio/src/test/java/com/google/sps/IsbnReaderTest.java
@@ -0,0 +1,37 @@
+package com.google.sps;
+
+import com.google.sps.data.IsbnReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * This is a test class for IsbnReader.java
+ * It only tests the basic functionality for reading from a CSV file.
+ * It does not check that the results match those from the Books API.
+ */
+@RunWith(JUnit4.class)
+public final class IsbnReaderTest {
+  private final String COURT_TITLE = "A Court of Wings and Ruin";
+  private final String COURT_ISBN = "9781619634497";
+  private final String HATE_TITLE = "The Hate U Give";
+  private final String HATE_ISBN = "9780062498557";
+  private Map<String, String> isbnMap;
+
+  @Test
+  public void basicSanityTests() {
+    try {
+      IsbnReader reader = new IsbnReader("src/test/java/com/google/sps/isbnTestFile.csv");
+      isbnMap = reader.makeIsbnMap();
+    } catch (Exception ex) {
+      Assert.fail("Could not open file. " + ex);
+    }
+    Assert.assertEquals(COURT_ISBN, isbnMap.get(COURT_TITLE));
+    Assert.assertEquals(HATE_ISBN, isbnMap.get(HATE_TITLE));
+  }
+}
