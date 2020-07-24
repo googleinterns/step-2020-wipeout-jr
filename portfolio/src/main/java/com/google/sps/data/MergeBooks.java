6ab92d024a9cc39f63b6f7362c8a7a3e31db47d6
Ankita Mitra <ankitamitra@berkeley.edu>
2020-07-24 03:43:33 +0000

    run clang format after merging

--- portfolio/src/main/java/com/google/sps/data/MergeBooks.java
+++ portfolio/src/main/java/com/google/sps/data/MergeBooks.java
@@ -7,20 +7,20 @@ import java.util.ArrayList;
 
 public class MergeBooks {
   /**
-  * This method merges two books that have the same title
-  * and ISBN and returns a new book combined from both of them
-  * @param googleApiBook: a book created by the bookResponse parser from the Books API
-  * Contains: title, categories, author, language, description, infoLink, pageCount, publishedDate,
-  * publisher, maturityRating, thumbnail, ISBN
-  * @param goodReadsBook: the book constructed from the csv file
-  * Contains: title, genre, reviews
-  */
+   * This method merges two books that have the same title
+   * and ISBN and returns a new book combined from both of them
+   * @param googleApiBook: a book created by the bookResponse parser from the Books API
+   * Contains: title, categories, author, language, description, infoLink, pageCount, publishedDate,
+   * publisher, maturityRating, thumbnail, ISBN
+   * @param goodReadsBook: the book constructed from the csv file
+   * Contains: title, genre, reviews
+   */
   public static Book merge(Book googleApiBook, Book goodReadsBook) {
     validate(googleApiBook);
     validate(goodReadsBook);
     validateISBN(googleApiBook.isbn());
-    
-    checkTitle(googleApiBook.title(),goodReadsBook.title());
+
+    checkTitle(googleApiBook.title(), goodReadsBook.title());
 
     // Get API data
     Book.Builder combinedBuilder = googleApiBook.toBuilder();
@@ -35,44 +35,44 @@ public class MergeBooks {
   }
 
   /**
-  * Checks the book titles, by comparing the shorter title with the first
-  * n characters of the longer title, so that if the series is included the
-  * books will still be able to be merged
-  * @param apiTitle: the title of the book parsed from the Google Books API
-  * @param goodReadsTitle: the title of the book parsed from the goodReads csv
-  */
-  private static void checkTitle(String apiTitle, String goodReadsTitle){
+   * Checks the book titles, by comparing the shorter title with the first
+   * n characters of the longer title, so that if the series is included the
+   * books will still be able to be merged
+   * @param apiTitle: the title of the book parsed from the Google Books API
+   * @param goodReadsTitle: the title of the book parsed from the goodReads csv
+   */
+  private static void checkTitle(String apiTitle, String goodReadsTitle) {
     String longerString;
     String shorterString;
-    if(apiTitle.length() > goodReadsTitle.length()){
-        longerString = apiTitle.toLowerCase();
-        shorterString = goodReadsTitle.toLowerCase();
-    }else{
-        longerString = goodReadsTitle.toLowerCase();
-        shorterString = apiTitle.toLowerCase();
+    if (apiTitle.length() > goodReadsTitle.length()) {
+      longerString = apiTitle.toLowerCase();
+      shorterString = goodReadsTitle.toLowerCase();
+    } else {
+      longerString = goodReadsTitle.toLowerCase();
+      shorterString = apiTitle.toLowerCase();
     }
-    String titleMismatchErrorMessage = "The books must be of the same title. Expected [%s] but got [%s]";
-    String substringOfLongTitle = longerString.substring(0,shorterString.length());
+    String titleMismatchErrorMessage =
+        "The books must be of the same title. Expected [%s] but got [%s]";
+    String substringOfLongTitle = longerString.substring(0, shorterString.length());
     titleMismatchErrorMessage = String.format(titleMismatchErrorMessage, goodReadsTitle, apiTitle);
     Preconditions.checkArgument(
-    shorterString.equals(substringOfLongTitle), titleMismatchErrorMessage);
-
+        shorterString.equals(substringOfLongTitle), titleMismatchErrorMessage);
   }
 
   /**
-  * Validates the book by checking that it has a title and the
-  * ISBN is in the correct format
-  * @param book: the book you want to validate
-  */
+   * Validates the book by checking that it has a title and the
+   * ISBN is in the correct format
+   * @param book: the book you want to validate
+   */
   private static void validate(Book book) {
     Preconditions.checkArgument(!book.title().equals(""), "The book's title was empty");
     Preconditions.checkArgument(!book.title().equals("N/A"), "The book's title was empty");
   }
 
   /**
-  * Validates the ISBN by checking that it is in the right format
-  * @param isbn: the ISBN you want to validate
-  */
+   * Validates the ISBN by checking that it is in the right format
+   * @param isbn: the ISBN you want to validate
+   */
   private static void validateISBN(String isbn) {
     String errorMessage = "Expected Google book-api based Book to contain the ISBN-13 populated.";
     Preconditions.checkArgument(!isbn.equals(""), errorMessage);
