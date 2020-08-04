d3649c376dfb04d2bd1450e0839004f10b7069c4
aamanhey <amanhey@google.com>
2020-08-04 00:12:00 +0000

    makes the list appear as a string

--- portfolio/src/main/webapp/book-list/book-list.component.js
+++ portfolio/src/main/webapp/book-list/book-list.component.js
@@ -5,7 +5,25 @@ angular.module('bookList').component('bookList', {
     var vm = this;
     $http.get('books').then(function(response) {
       vm.bookList = response.data;
+      for(i=0;i<vm.bookList.length;i++){
+          vm.bookList[i].genre = formatList(vm.bookList[i].genre);
+      }
     })
+
+    function formatList(list) {
+      var formattedList = '';
+      var i;
+      for (i = 0; i < list.length; i++) {
+        if (i == list.length - 1) {
+          var element = list[i];
+        } else {
+          var element = list[i] + ', ';
+        }
+        formattedList += element;
+      }
+      return formattedList;
+    }
+
   },
   controllerAs: 'bookListCtrl'
 });
